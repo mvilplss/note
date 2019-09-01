e11dd8183c0386532e371d162d3d22373b7b2fbc
sanxing <sanhang@mhc.com>
2019-09-01 22:46:15 +0800

    xx

--- code/src/main/java/JavaCompilerDemo.java
+++ code/src/main/java/JavaCompilerDemo.java
@@ -14,14 +14,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
 
-/**
- * All rights Reserved, Designed By www.maihaoche.com
- *
- * @author: 三行（sanxing@maihaoche.com）
- * @date: 2019/9/1
- * @Copyright: 2017-2020 www.maihaoche.com Inc. All rights reserved.
- * 注意：本内容仅限于卖好车内部传阅，禁止外泄以及用于其他的商业目
- */
+
 @Slf4j
 public class JavaCompilerDemo extends BaseDemo {
 
--- code/src/main/java/SimpleBean.java
+++ code/src/main/java/SimpleBean.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class SimpleBean {
+    List list = new ArrayList();
     public String whoiam(){
       return "my name is SimpleBean for testing.";
     }
